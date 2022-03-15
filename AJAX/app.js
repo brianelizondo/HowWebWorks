92587c46ac19d0f6ca4e643f91275702755f9c96
Brian Elizondo <brian_elizondo@msn.com>
2022-03-14 21:46:55 -0400

    Part 3: Removing GIFs => Add a button next to the form which, when clicked, will remove all GIFs from the page. Update all code to jQuery version

--- AJAX/app.js
+++ AJAX/app.js
@@ -6,13 +6,16 @@
 // You can click on this URL and see the JSON you will get back. To view this in a nicer format, we highly recommend using the JSON Viewer chrome extension. This is also the data you should console.log when the form is submitted.
 
 let api_key = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
-const giphy_images = document.querySelector('#giphy_images');
+const $giphy_images = $('#giphy_images');
 
 async function getGiphy(search){
     try {
         const url = 'http://api.giphy.com/v1/gifs/search';
         const resp = await axios.get(url, {params: {q: search, api_key }});
-        appendGIF(resp.data.data[0].images.original.url);
+        const min = 0;
+        const max = resp.data.data.length;
+        const randomGIF = Math.floor(Math.random() * (max - min) + min);
+        appendGIF(resp.data.data[randomGIF].images.original.url);
     } catch (e) {
         alert("Search term not found for GIPHY");
     }
@@ -21,15 +24,23 @@ async function getGiphy(search){
 // Part 2: Appending GIFs
 // Now that you’re communicating properly with the API, you should do something more interesting with the response data. Instead of logging it, grab a GIF from the response data and append the GIF to the page. Ensure that if you submit the form multiple times, you’ll get multiple GIFs showing up.
 function appendGIF(url){
-    let gifImage = document.createElement('img');
-    gifImage.setAttribute('src', url);
-    giphy_images.append(gifImage);
+    let $gifImage = $('<img>');
+    $gifImage.attr('src', url);
+    $giphy_images.append($gifImage);
 }
 
-const search_form = document.querySelector('#search_form');
-const search_term = document.querySelector('#search');
-search_form.addEventListener('submit', function(e){
+// Part 3: Removing GIFs
+// Add a button next to the form which, when clicked, will remove all GIFs from the page.
+const $btnRemove = $('#btn_remove');
+$btnRemove.on('click', function(){
+    let $gifShowing = $('img');
+    $gifShowing.remove();
+});
+
+const $search_form = $('#search_form');
+const $search_term = $('#search');
+$search_form.on('submit', function(e){
     e.preventDefault();
-    getGiphy(search_term.value);
-    search_term.value = '';
+    getGiphy($search_term.val());
+    $search_term.val('');
 });
